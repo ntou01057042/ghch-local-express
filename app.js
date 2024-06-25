fa87a04d910fc1f9f89f5ec21c8e1a78cae3115c
ntou01057042 <134361473+ntou01057042@users.noreply.github.com>
2024-06-25 20:53:33 +0800

    Enable all CORS Requests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var repoRouter = require('./routes/repo');
@@ -18,5 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/repo', repoRouter);
 app.use('/collab', collaboratorRouter);
+
+app.use(cors())
 
 module.exports = app;
