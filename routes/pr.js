4f2919f8074f93120c66808c743b09e0db16f745
ntou01057042 <134361473+ntou01057042@users.noreply.github.com>
2024-09-03 01:17:40 +0800

    Add endpoint to get a pull request

--- routes/pr.js
+++ routes/pr.js
@@ -11,7 +11,7 @@ router.get('/list', async function (req, res, next) {
     const response = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
         owner: req.query.owner,   // The account owner of the repository.
         repo: req.query.repo,   // The name of the repository without the .git extension.
-        state: req.query.state,   // Either open, closed, or all to filter by state.
+        state: 'all',   // Either open, closed, or all to filter by state.
         headers: {
             'X-GitHub-Api-Version': '2022-11-28',
             'accept': 'application/vnd.github+json'
@@ -21,9 +21,39 @@ router.get('/list', async function (req, res, next) {
     let prs = [];
     for (const i in response.data) {
         console.log(response.data[i]);
-        prs.push({ id: response.data[i].id, title: response.data[i].title });
+        prs.push({
+            id: response.data[i].id,
+            number: response.data[i].number,
+            state: response.data[i].state,
+            title: response.data[i].title,
+        });
     }
     res.send(prs)
+});
+
+/* Get a pull request */
+// https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#list-pull-requests
+router.get('/get', async function (req, res, next) {
+    const octokit = new Octokit({
+        // auth: req.query.token
+    })
+    const response = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+        owner: req.query.owner,   // The account owner of the repository.
+        repo: req.query.repo,   // The name of the repository without the .git extension.
+        pull_number: req.query.pull_number,   // The number that identifies the pull request.
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28',
+            'accept': 'application/vnd.github+json'
+        }
+    })
+    console.log(response);
+    res.send({
+        number: response.data.number,
+        state: response.data.state,
+        description: response.data.body,
+        head: response.data.head.ref,
+        base: response.data.base.ref,
+    });
 });
 
 module.exports = router;
