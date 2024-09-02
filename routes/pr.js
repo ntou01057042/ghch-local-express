56059976b4a319a1b11f2abd427b138fa6fbbf26
ntou01057042 <134361473+ntou01057042@users.noreply.github.com>
2024-09-03 01:33:59 +0800

    Add endpoint to list all pr comments

--- routes/pr.js
+++ routes/pr.js
@@ -46,7 +46,6 @@ router.get('/get', async function (req, res, next) {
             'accept': 'application/vnd.github+json'
         }
     })
-    console.log(response);
     res.send({
         number: response.data.number,
         state: response.data.state,
@@ -54,6 +53,34 @@ router.get('/get', async function (req, res, next) {
         head: response.data.head.ref,
         base: response.data.base.ref,
     });
+});
+
+/* List issue comments */
+// https://docs.github.com/en/rest/issues/comments?apiVersion=2022-11-28#list-issue-comments
+router.get('/comments', async function (req, res, next) {
+    const octokit = new Octokit({
+        // auth: req.query.token
+    })
+    const response = await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+        owner: req.query.owner,   // The account owner of the repository.
+        repo: req.query.repo,   // The name of the repository without the .git extension.
+        issue_number: req.query.pull_number,   // The number that identifies the pull request.
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28',
+            'accept': 'application/vnd.github+json'
+        }
+    })
+    let comments = [];
+    for (const i in response.data) {
+        console.log(response.data[i]);
+        comments.push({
+            id: response.data[i].id,
+            user: response.data[i].user.login,
+            created_at: response.data[i].created_at,
+            body: response.data[i].body,
+        });
+    }
+    res.send(comments);
 });
 
 module.exports = router;
