d7e864e3281533468f5da92cedc533e3a642a3ba
ntou01057042 <134361473+ntou01057042@users.noreply.github.com>
2024-10-18 22:58:21 +0800

    Add endpoint to delete merged branch

---
+++ routes/branch.js
@@ -0,0 +1,23 @@
+var express = require('express');
+const { Octokit } = require("@octokit/rest");
+var router = express.Router();
+
+/* Delete a reference */
+// https://docs.github.com/en/rest/git/refs?apiVersion=2022-11-28#delete-a-reference
+router.post('/delete', async function (req, res, next) {
+    const octokit = new Octokit({
+        auth: req.query.token
+    })
+    const response = await octokit.request('DELETE /repos/{owner}/{repo}/git/refs/{ref}', {
+        owner: req.query.owner,
+        repo: req.query.repo,
+        ref: 'heads/' + req.query.ref,
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+    console.log(response);
+    res.send(response)
+});
+
+module.exports = router;
